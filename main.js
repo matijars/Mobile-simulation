4815f944d79221b0fa15a4c3770d42eefb81aad1
Matija <matijamilosevic1988@gmail.com>
2019-11-22 17:14:01 +0100

    active nav icons

--- main.js
+++ main.js
@@ -32,9 +32,49 @@ landscape.addEventListener('click', ()=> {
 });
 
 // ICONS 
-
 document.querySelectorAll('.menu-icon').forEach(item => {
   item.addEventListener('click', event => {
     item.classList.toggle('menu-item-active');
   })
 })
+
+// WIFI ICON 
+const menuWifi = document.querySelector('.menu-wifi')
+const navWifi = document.querySelector('.nav-wifi')
+
+
+menuWifi.addEventListener('click', ()=> {
+  navWifi.classList.toggle('hide')
+});
+
+// LOCATION ICON
+const menuLocation = document.querySelector('.menu-location')
+const navLocation = document.querySelector('.nav-location')
+
+menuLocation.addEventListener('click', ()=> {
+  navLocation.classList.toggle('show')
+});
+
+// BLUETOOTH ICON
+const menuBluetooth = document.querySelector('.menu-bluetooth')
+const navBluetooth = document.querySelector('.nav-bluetooth')
+
+menuBluetooth.addEventListener('click', ()=> {
+  navBluetooth.classList.toggle('show')
+});
+
+// BLUETOOTH ICON
+const menuVolume = document.querySelector('.menu-volume')
+const navVolume = document.querySelector('.nav-volume')
+
+menuVolume.addEventListener('click', ()=> {
+  navVolume.classList.toggle('show')
+});
+
+// BLUETOOTH ICON
+const menuPlane = document.querySelector('.menu-plane')
+const navPlane = document.querySelector('.nav-plane')
+
+menuPlane.addEventListener('click', ()=> {
+  navPlane.classList.toggle('show')
+});
